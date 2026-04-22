cbf52547f4e8f9d677862eed376da26546952fb8
magaraj <magarajcb@gmail.com>
2026-04-22 16:37:06 +0530

    update URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: 'https://newsalertapp-backend.onrender.com',
     withCredentials: true,
     headers: { 'Content-Type': 'application/json' }
 });
